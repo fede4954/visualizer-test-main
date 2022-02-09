541e01806deb140abe998da6c4af1e5c59754d8d
Fede <f.gomezlara@outlook.es>
2022-02-09 18:22:19 +0100

    store points and materials data in states

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@ import db from '../firebase/config'
 import { collection, getDocs } from 'firebase/firestore/lite'
 import { useState, useEffect } from 'react'
 
+
 const Home = () => {
+  const [points, setPoints] = useState('')
+  const [materials, setMaterials] = useState('')
+  const [loading, setLoading] = useState(true)
+
+
   const getList = async (colName) => {
     const col = collection(db, colName)
     const snapshot = await getDocs(col)
@@ -12,9 +18,21 @@ const Home = () => {
 
 
   useEffect(() => {
-    console.log('points', getList('points'))
-    console.log('materials', getList('materials'))
+    const getAllData = async () => {
+      const pnts = await getList('points')
+      const mats = await getList('materials')
+      setPoints(pnts)
+      setMaterials(mats)
+      setLoading(false)
+    }
+
+    getAllData()
   }, [])
+
+  useEffect(() => {
+    console.log(points)
+    console.log(materials)
+  }, [materials, points])
 
 
   return (
