29ebcf6093e0a8b17c82aa2e8c4e9ed4766826d9
Fede <f.gomezlara@outlook.es>
2022-02-09 18:05:15 +0100

    get points and materials from firebase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function Home () {
+import db from '../firebase/config'
+import { collection, getDocs } from 'firebase/firestore/lite'
+import { useState, useEffect } from 'react'
+
+const Home = () => {
+  const getList = async (colName) => {
+    const col = collection(db, colName)
+    const snapshot = await getDocs(col)
+    const list = snapshot.docs.map((doc) => doc.data())
+    return list
+  }
+
+
+  useEffect(() => {
+    console.log('points', getList('points'))
+    console.log('materials', getList('materials'))
+  }, [])
+
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       ESTUDIO CACTUS VISUALIZER TEST
     </div>
   )
 }
+
+export default Home
