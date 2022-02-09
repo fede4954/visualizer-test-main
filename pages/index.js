2f9ce627567833e031214f024aff04311aaeac88
Fede <f.gomezlara@outlook.es>
2022-02-09 20:35:10 +0100

    test image layering manually

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,17 @@ const Home = () => {
   const [loading, setLoading] = useState(true)
 
 
-  const getList = async (colName) => {
+  const getData = async (colName) => {
     const col = collection(db, colName)
     const snapshot = await getDocs(col)
-    const list = snapshot.docs.map((doc) => doc.data())
-    return list
+    return snapshot.docs.map((doc) => doc.data())
   }
 
 
   useEffect(() => {
     const getAllData = async () => {
-      const pnts = await getList('points')
-      const mats = await getList('materials')
+      const pnts = await getData('points')
+      const mats = await getData('materials')
       setPoints(pnts)
       setMaterials(mats)
       setLoading(false)
@@ -29,17 +28,23 @@ const Home = () => {
     getAllData()
   }, [])
 
-  useEffect(() => {
-    console.log(points)
-    console.log(materials)
-  }, [materials, points])
-
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      ESTUDIO CACTUS VISUALIZER TEST
+    <div className="flex flex-col items-center justify-center">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/projects%2FdorptVQTHsbkYC60NSlt%2Fscenes%2F1567170849457-base?alt=media&token=cf8bcee2-bf89-4fd9-8bfd-9d4462348844" 
+          alt="Base image"
+          className="absolute top-0 left-0"
+        />
+
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/materials%2F1563187566938%2FdorptVQTHsbkYC60NSlt-1567170849457-mTrveExUlJpsEInPt1KX?alt=media&token=aca48ffd-c464-4936-b85c-7249bbb64ea9" 
+          alt="Base image"
+          className="absolute top-0 left-0 z-50"
+        />
     </div>
   )
 }
 
 export default Home
+
