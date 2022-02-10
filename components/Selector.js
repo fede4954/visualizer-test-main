659c4667338e4499af104b62d8f4dbd0ae78c0de
Fede <f.gomezlara@outlook.es>
2022-02-10 15:04:55 +0100

    position and size selector

--- components/Selector.js
+++ components/Selector.js
@@ -1,7 +1,7 @@
 const Selector = (props) => {
     const { materials, setMaterial } = props
 
-    return <ul className="absolute md:w-[300px] h-[425px] left-[75%] bottom-[2.5%] md:bottom-[25%] 2xl:left-[70%]">
+    return <ul className="z-50 w-[100px] h-[225px] md:w-[145px] md:h-[300px] lg:mr-[4.5%] xl:w-[200px] xl:h-[380px] xl:mr-[2.25%] 2xl:mr-[12.5%]">
         {materials.map((layer, index) => {
             return <li>
                 <button
@@ -11,13 +11,12 @@ const Selector = (props) => {
                     key={index + Date.now()}
                 >
                     <div className="flex justify-between items-center w-full my-2">
-                        <div className="w-[150px]">
-                            <p>{layer.name}</p>
+                        <div className="w-1/2">
+                            <p className="text-xs">{layer.name}</p>
                         </div>
-                        <div className="rounded border-4 border-white">
+                        <div className="rounded border-4 border-white w-1/2">
                             <img
                                 src={layer.thumbnail}
-                                className="w-[100px]"
                             />
                         </div>
                     </div>
--- pages/index.js
+++ pages/index.js
@@ -41,44 +41,8 @@ const Home = () => {
   }, [])
 
 
-  return <div className="container">
+  return <div className="min-h-screen flex flex-col justify-center items-end">
     {!loading && <>
-      <img
-        src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/projects%2FdorptVQTHsbkYC60NSlt%2Fscenes%2F1567170849457-base?alt=media&token=cf8bcee2-bf89-4fd9-8bfd-9d4462348844"
-        alt="Base image"
-        className="fixed top-0 left-0 w-full h-full object-contain"
-        onClick={() => checkMenus()}
-      />
-
-      {floor && <img
-        src={floor}
-        alt="Floor"
-        className="fixed top-0 left-0 w-full h-full object-contain"
-        onClick={() => checkMenus()}
-      />}
-
-      {kitchen && <img
-        src={kitchen}
-        alt="Kitchen"
-        className="fixed top-0 left-0 w-full h-full object-contain"
-        onClick={() => checkMenus()}
-      />}
-
-      {touch && <Touch
-        menu={floorSelector}
-        setMenu={setFloorSelector}
-        position={"absolute left-[25%] bottom-[35%] md:bottom-[27.5%] lg:left-[35%] lg:bottom-[5%]"}
-        setTouch={setTouch}
-      />}
-
-
-      {touch && <Touch
-        menu={kitchenSelector}
-        setMenu={setKitchenSelector}
-        position={"absolute left-[75%] bottom-[55%] lg:left-[70%] lg:bottom-[60%]"}
-        setTouch={setTouch}
-      />}
-
       {floorSelector && <Selector
         materials={materials[0].materials}
         setMaterial={setFloor}
@@ -88,7 +52,43 @@ const Home = () => {
         materials={materials[1].materials}
         setMaterial={setKitchen}
       />}
+      <div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/projects%2FdorptVQTHsbkYC60NSlt%2Fscenes%2F1567170849457-base?alt=media&token=cf8bcee2-bf89-4fd9-8bfd-9d4462348844"
+          alt="Base image"
+          className="fixed top-0 left-0 w-full h-full object-contain"
+          onClick={() => checkMenus()}
+        />
 
+        {floor && <img
+          src={floor}
+          alt="Floor"
+          className="fixed top-0 left-0 w-full h-full object-contain"
+          onClick={() => checkMenus()}
+        />}
+
+        {kitchen && <img
+          src={kitchen}
+          alt="Kitchen"
+          className="fixed top-0 left-0 w-full h-full object-contain"
+          onClick={() => checkMenus()}
+        />}
+
+        {touch && <Touch
+          menu={floorSelector}
+          setMenu={setFloorSelector}
+          position={"absolute left-[25%] bottom-[35%] md:bottom-[27.5%] lg:left-[35%] lg:bottom-[5%]"}
+          setTouch={setTouch}
+        />}
+
+
+        {touch && <Touch
+          menu={kitchenSelector}
+          setMenu={setKitchenSelector}
+          position={"absolute left-[75%] bottom-[55%] lg:left-[70%] lg:bottom-[60%]"}
+          setTouch={setTouch}
+        />}
+      </div>
     </>}
   </div>
 }
